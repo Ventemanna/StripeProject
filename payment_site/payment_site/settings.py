71b014790b5082c30d4d0d53681100b1ee1ba9c0
ventemanna <ventumanna@gmail.com>
2025-08-08 21:10:34 +0300

    Немного внешнего оформления

--- payment_site/payment_site/settings.py
+++ payment_site/payment_site/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    str(BASE_DIR) + "/payment/static/"
+]
 
 SECRET_STRIPE_KEY = os.getenv('SECRET_STRIPE_KEY')
 PUBLIC_STRIPE_KEY = os.getenv('PUBLIC_STRIPE_KEY')
