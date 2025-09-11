b625fa5c1acbf8f788cbe2d0a8148a5700ec4473
ventemanna <ventumanna@gmail.com>
2025-09-11 15:45:48 +0300

    readme.md

--- payment_site/payment_site/settings.py
+++ payment_site/payment_site/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r8=_rvhoq+(=(2^gz)#1@jm9fg#8xze&$8x0t$(-lft1iuolbs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
